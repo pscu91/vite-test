753cbc23491257bfc2dc00d05bec599612c691bc
pscu91 <91787174+pscu91@users.noreply.github.com>
2025-02-07 17:19:11 +0900

    tailwind-setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#21BF48",
+        mainText: "#000000",
+        subText: "#767676",
+        accentText: "#EB5757",
+        background: "#f2f2f2",
+      },
+      fontFamily: {
+        // spoqa: ["SpoqaHanSansNeo-Regular"],
+        // spoqaMedium: ["SpoqaHanSansNeo-Medium"],
+        // spoqaBold: ["SpoqaHanSansNeo-Bold"],
+      },
+    },
+    screens: {
+      ss: "480px",
+      sm: "620px",
+      sl: "768px",
+      md: "1060px",
+      lg: "1200px",
+    },
+  },
+  corePlugins: {
+    preflight: true,
   },
   plugins: [],
-}
-
+};
