c719ebf6f7b466eb9bf0a7b59e7b90c0c990156e
pscu91 <91787174+pscu91@users.noreply.github.com>
2025-02-10 08:33:14 +0900

    prettier setup

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,12 @@ function App() {
       <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
         <img class="size-12 shrink-0" src={viteLogo} alt="Vite Logo" />
         <div class="flex-col max-auto text-left">
-          <div class="text-xl font-medium text-black dark:text-white">Vite</div>
-          <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+          <div class="text-xl font-medium text-black dark:text-white">
+            ChitChat
+          </div>
+          <p class="text-gray-500 dark:text-gray-400">
+            You have a new message!
+          </p>
         </div>
       </div>
       {/* <div>
