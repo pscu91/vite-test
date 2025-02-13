28a5ab1a6e5342d89a259416be7886cf23c1a333
pscu91 <91787174+pscu91@users.noreply.github.com>
2025-02-13 12:02:58 +0900

    select chevron edit

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -47,7 +47,7 @@ function Contributors() {
             <div className="relative">
               <FontAwesomeIcon
                 icon={faChevronDown}
-                className="absolute right-3 top-3 z-10 transform select-none pt-0.5 text-purple-600"
+                className="pointer-events-none absolute right-3 top-3 z-10 transform pt-0.5 text-purple-600"
               />
               <select
                 class="relative block w-full appearance-none rounded border border-purple-200 bg-purple-50 px-4 py-3 pr-8 leading-tight text-slate-800 focus:border-slate-800 focus:bg-white focus:outline-none"
