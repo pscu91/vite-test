461c23c1ee3adddd63b01679bb85d6bdc5936631
pscu91 <91787174+pscu91@users.noreply.github.com>
2025-02-18 11:48:44 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./vite-test/",
+  base: "/vite-test/",
   plugins: [react()],
 });
