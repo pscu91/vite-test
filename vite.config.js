3702decc298cbd71dabd2a4c79330f7346bc9869
pscu91 <91787174+pscu91@users.noreply.github.com>
2025-02-28 13:15:06 +0900

    github build test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // GitHub Pages 배포 시 사용할 레포지토리 이름
 const repositoryName = "vite-test";
@@ -8,8 +8,25 @@ const repositoryName = "vite-test";
 const base =
   process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "/";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base,
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/vite-test/" : "/",
+  define: {
+    "process.env.VITE_API_KEY": JSON.stringify(process.env.VITE_API_KEY),
+    "process.env.VITE_AUTH_DOMAIN": JSON.stringify(
+      process.env.VITE_AUTH_DOMAIN,
+    ),
+    "process.env.VITE_PROJECT_ID": JSON.stringify(process.env.VITE_PROJECT_ID),
+    "process.env.VITE_STORAGE_BUCKET": JSON.stringify(
+      process.env.VITE_STORAGE_BUCKET,
+    ),
+    "process.env.VITE_MESSAGING_SENDER_ID": JSON.stringify(
+      process.env.VITE_MESSAGING_SENDER_ID,
+    ),
+    "process.env.VITE_APP_ID": JSON.stringify(process.env.VITE_APP_ID),
+    "process.env.VITE_MEASUREMENT_ID": JSON.stringify(
+      process.env.VITE_MEASUREMENT_ID,
+    ),
+  },
 });
