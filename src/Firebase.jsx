561f5f279e9f8a24003be3ec0236bda3831a5d77
pscu91 <91787174+pscu91@users.noreply.github.com>
2025-02-28 11:32:54 +0900

    deploy debug setting update

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -12,6 +12,17 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
+// 환경 변수 확인을 위한 디버깅
+console.log("Firebase Config:", {
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+  appId: firebaseConfig.appId,
+  measurementId: firebaseConfig.measurementId,
+});
+
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
 
